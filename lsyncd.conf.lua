0e573355f02a5d8a723fdd73e8fb20f2af9655c2
atyenoria <eigotyoubunn30@gmail.com>
2015-05-28 04:21:18 +0900

    ssh_config

--- lsyncd.conf.lua
+++ lsyncd.conf.lua
@@ -8,6 +8,7 @@ sync{
         default.rsync,
         source="/home/vagrant/test_sor",
         target="vagrant@192.168.200.200:/home/vagrant/test_des/", 
+        delete="running",
  rsync = {
         rsh = "/usr/bin/ssh -i /home/vagrant/.ssh/id_rsa -o StrictHostKeyChecking=no",
        compress = false,
